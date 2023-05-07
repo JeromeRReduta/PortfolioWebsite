44b4078a2d5ff720a80da680e138ac671f19bf7c
Jerome Reduta <JRReduta@yahoo.com>
2023-05-07 14:07:01 -0700

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES ('a', 'https://github.com/JeromeRReduta', 'a, b, c',
+    'sample 0');
+INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES ('b', 'https://github.com/JeromeRReduta', 'a, b, c',
+    'sample 1');
+INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES ('c', 'https://github.com/JeromeRReduta', 'a, b, c',
+    'sample 2');
+INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES ('d', 'https://github.com/JeromeRReduta', 'a, b, c',
+    'sample 3');
+
---
+++ src/test/java/com/jerome/portfoliowebsite/PortfolioWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerome.portfoliowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
