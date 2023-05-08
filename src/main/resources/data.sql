04ea00edd0f5c40c30bef52b4c8065770d534133
Jerome Reduta <JRReduta@yahoo.com>
2023-05-07 19:15:11 -0700

    Added data for portfolio entry

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,8 @@
-INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES ('Portfolio', '/all-projects/portfolio', 'a, b, c',
-    'sample 0');
+INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES (
+    'Portfolio',
+    'https://github.com/JeromeRReduta/PortfolioWebsite',
+    'Spring MVC, Spring Boot, HTML, Model-View-Controller, Java, SQL, Embedded Databases',
+    '	I’ve heard it’s important to create a portfolio site, so my first recent project *is this site*. It’s in Spring Boot, using a Model-View-Controller pattern and an embedded database. My favorite part is that every page describing each project (e.g. this page) is all dynamically built from the same template. That means I can add another page with 5 lines of Java and 1 new SQL statement.'
+    );
 INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES ('Java Follow-Along', 'https://github.com/JeromeRReduta', 'a, b, c',
     'sample 1');
-INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES ('c', 'https://github.com/JeromeRReduta', 'a, b, c',
-    'sample 2');
-INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES ('d', 'https://github.com/JeromeRReduta', 'a, b, c',
-    'sample 3');
-
