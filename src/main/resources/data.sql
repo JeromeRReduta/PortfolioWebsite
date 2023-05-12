c49a2dd58526db5ba55919ca06a991f7fef28eee
Jerome Reduta <JRReduta@yahoo.com>
2023-05-12 14:18:36 -0700

    Updated content for portfolio site entry

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,6 +14,6 @@ INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES (
 INSERT INTO PROJECT (NAME, GITHUB, SKILLS_USED, SUMMARY) VALUES (
     'Portfolio',
     'https://github.com/JeromeRReduta/PortfolioWebsite',
-    'Spring MVC, Spring Boot, HTML, Model-View-Controller, Java, SQL, Embedded Databases',
-    '	I’ve heard it’s important to create a portfolio site, so my first recent project *is this site*. It’s in Spring Boot, using a Model-View-Controller pattern and an embedded database. My favorite part is that every page describing each project (e.g. this page) is all dynamically built from the same template. That means I can add another page with 5 lines of Java and 1 new SQL statement.'
+    'Spring MVC, Spring Boot, HTML, CSS, Bootstrap, Thymeleaf, Model-View-Controller, Java, SQL, Embedded Databases, AWS Elastic Beanstalk',
+    '	I’ve heard it’s important to create a portfolio site, so my first recent project *is this site*. It’s in Spring Boot, using a Model-View-Controller pattern and an embedded database, hosted on AWS Elastic Beanstalk. My favorite part is that every page describing each project (e.g. this page) is all dynamically built from the same template. That means I can add another page with 5 lines of Java and 1 new SQL statement.'
 );
